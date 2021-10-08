cdc92f32233f534604f05de495973c71f5d2062f
lxbpxylps@126.com <lxbpxylps@126.com>
2021-10-08 09:14:10 +0800

    删除冗余代码

--- bot.py
+++ bot.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-
 import nonebot
 from nonebot import logger
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
-
-# 若 data 文件夹不存在则创建
-if os.path.exists('data') == False:
-    os.mkdir('data')
 
 # dev 环境下监控 src config 文件夹变动
 nonebot.init(fastapi_reload_dirs=['src', 'config'])
